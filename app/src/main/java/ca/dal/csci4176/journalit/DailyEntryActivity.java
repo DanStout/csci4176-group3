1ec319ea360788d7f13d33b7f736b719d4a072bc
Tyler <byte@MacBook>
2017-03-15 13:34:09 -0300

    Added second activity

---
+++ app/src/main/java/ca/dal/csci4176/journalit/DailyEntryActivity.java
@@ -0,0 +1,13 @@
+package ca.dal.csci4176.journalit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DailyEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_entry);
+    }
+}
